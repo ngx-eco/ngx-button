72d8ac63377aa98c0f678d0ba02fe6b2d3cd42ae
fil0157 <56268081+fil0157@users.noreply.github.com>
2021-01-20 11:10:01 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngx-button';
 
   click() {
     console.log('click');
   }
+
 }
