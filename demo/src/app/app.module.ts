b21c0be047b25ef84d324041d54b2a3a886681e0
fil0157 <56268081+fil0157@users.noreply.github.com>
2021-01-20 11:20:18 +0300

    Demo app added

---
+++ demo/src/app/app.module.ts
@@ -0,0 +1,20 @@
+// Angular
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+// App
+import { AppComponent } from './app.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
