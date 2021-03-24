651a19f253e4cf3586cafd56c5080f68c059bcb0
fil0157 <56268081+fil0157@users.noreply.github.com>
2021-03-24 17:55:46 +0300

    App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,9 @@ import { Component } from '@angular/core';
 
 
 @Component({
-  selector: 'app-root',
+  selector: 'root',
   templateUrl: './app.component.pug',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  click() {
-    console.log('click');
-  }
-
 }
