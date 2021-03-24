1f6c2934ec9ecca9d6c7e2f59ef8bca9db06d14d
fil0157 <56268081+fil0157@users.noreply.github.com>
2021-03-25 00:00:40 +0300

    Demo service

---
+++ demo/src/app/services/demo/demo.service.spec.ts
@@ -0,0 +1,20 @@
+// Angular
+import { TestBed } from '@angular/core/testing';
+
+// App
+import { DemoService } from './demo.service';
+
+
+
+// describe('DemoService', () => {
+//   let service: DemoService;
+
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({});
+//     service = TestBed.inject(DemoService);
+//   });
+
+//   it('should be created', () => {
+//     expect(service).toBeTruthy();
+//   });
+// });
